a9ae1193e4bc871c2241a87d070564998316a95a
chui <1229059439@qq.com>
2021-04-16 14:10:10 +0800

    等待编写对用户提交表单的处理

--- functional_tests.py
+++ functional_tests.py
@@ -4,6 +4,7 @@ import unittest
 from selenium.webdriver.common.keys import Keys
 import time
 
+
 class NewVisitorTest(unittest.TestCase):
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
@@ -19,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('TO-DO',header_text)
+        self.assertIn('To-Do', header_text)
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
@@ -35,9 +36,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table."
         )
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -50,6 +51,7 @@ class NewVisitorTest(unittest.TestCase):
         # explanatory text to that effect.
 
         # She visits that URL - her to-do list is still there.
+
 
 # Satisfied, she goes back to sleep
 if __name__ == '__main__':
