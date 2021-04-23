72367c1da9b95f232f7a939785a6c7729b08f147
chui <1229059439@qq.com>
2021-04-23 14:01:07 +0800

    deal with one item

--- lists/tests.py
+++ lists/tests.py
@@ -13,6 +13,11 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/',data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response,'home.html')
+
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
 
@@ -22,3 +27,4 @@ class HomePageTest(TestCase):
         self.assertTrue(html.strip().endswith('</html>'))
 
         self.assertTemplateUsed(response, 'home.html')
+
